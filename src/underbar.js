4eae987f4cc328f3837540e9638854b1b9e0bd23
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-05-04 13:24:46 -0400

    Fixed "Last" and "Identity"

--- src/underbar.js
+++ src/underbar.js
@@ -8,6 +8,7 @@ var _ = {};
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -38,6 +39,11 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === 0) {
+      return [];
+    } else {
+      return n === undefined ? array.slice(-1)[0] : array.slice(-1 * n);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
