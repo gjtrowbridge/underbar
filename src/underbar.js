1ca396de9f655385777a586f6ccccd90b2e877b2
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-05-06 14:35:13 -0400

    Extend is now passing

--- src/underbar.js
+++ src/underbar.js
@@ -268,6 +268,13 @@ var _ = {};
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i=1; i<arguments.length; i++) {
+      var new_obj = arguments[i];
+      for (var key in new_obj) {
+        obj[key] = new_obj[key];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
